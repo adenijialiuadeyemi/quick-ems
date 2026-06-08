cc7a4a6914ba8972fda0b56577b160813dcc8d84
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2026-06-08 17:05:09 +0100

    Sideba UI completed

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="flex h-screen bg-gradient-to-br from-slate-50 via-white to-indigo-50/30">
+      <Sidebar />
+      <main className="flex-1 overflow-y-auto">
+        <div className="p-4 sm:p-6 lg:p-8 max-w-6xl mx-auto">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
